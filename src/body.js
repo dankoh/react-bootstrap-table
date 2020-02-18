a22996cd61de2cf310b7bacba176d7537c06fc7e
Daniel Kohn <dkohn@smail.uni-koeln.de>
2020-02-18 18:42:59 +0100

    revert

--- src/body.js
+++ src/body.js
@@ -11,15 +11,7 @@ import RowSection from './row/row-section';
 import Const from './const';
 import withRowSelection from './row-selection/row-consumer';
 import withRowExpansion from './row-expand/row-consumer';
-import { VariableSizeList as List } from 'react-window';
-import { CellMeasurer, CellMeasurerCache } from 'react-virtualized';
-
-const cache = new CellMeasurerCache({
-  defaultWidth: 100,
-  minWidth: 75,
-  fixedHeight: true
-});
-
+import { FixedSizeList as List } from 'react-window';
 
 class Body extends React.Component {
   constructor(props) {
@@ -97,8 +89,8 @@ class Body extends React.Component {
       }
 
 
-      const Row = ({  index, isScrolling, key, parent, style }) => {
-        key = _.get(data[index], keyField);
+      const Row = ({ index, style }) => {
+        const key = _.get(data[index], keyField);
         const row = data[index];
         const baseRowProps = {
           key,
@@ -118,17 +110,8 @@ class Body extends React.Component {
         baseRowProps.style = { ...style, ...UserStyle };
         baseRowProps.className = (_.isFunction(rowClasses) ? rowClasses(data[index], index) : rowClasses);
         return (
-            <CellMeasurer
-                cache={cache}
-                columnIndex={0}
-                key={key}
-                parent={parent}
-                rowIndex={index}
-            >
-              <this.RowComponent {...baseRowProps} />
-            </CellMeasurer>
+            <this.RowComponent {...baseRowProps} />
         );
-
       };
 
       const TableBody = ({ children, ...props }) => (
@@ -142,6 +125,7 @@ class Body extends React.Component {
               className="List"
               height={300}
               itemCount={data.length}
+              itemSize={50}
               innerElementType={TableBody}
           >
             {Row}
